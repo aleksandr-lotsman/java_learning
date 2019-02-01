1df912562e915a8cc065b1d4890da8818f8cc862
Alexandr Lotsman <lotsman1993@gmail.com>
2019-02-01 19:08:35 +0200

    Finished Homework 1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,15 +13,11 @@ public class MyFirstProgram {
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-		Point p1 = new Point();
-		p1.x = 5;
-		p1.y = 1;
+		Point p1 = new Point(0,4);
+		Point p2 = new Point(4, 4);
 
-		Point p2 = new Point();
-		p2.x = -3;
-		p2.y = 1;
 
-		System.out.println("Расстояние между точкачи c координатами (" + p1.x + ", " + p1.y + "), (" + p2.x + ", " + p2.y + ") = " + distance(p1, p2));
+		System.out.println("Расстояние между точкачи c координатами (" + p1.x + ", " + p1.y + "), (" + p2.x + ", " + p2.y + ") = " + p1.distance(p2));
 
 	}
 
@@ -29,13 +25,7 @@ public class MyFirstProgram {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public  static double distance (Point p1, Point p2){
 
-		double distance;
-		distance = Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-		return distance;
-
-	}
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,4 +4,16 @@ public class Point {
 
     double x;
     double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance (Point p){
+
+        return Math.sqrt((p.x - x)*(p.x - x) + (p.y - y) * (p.y - y));
+
+    }
+
 }
