a41116d5b768e876cdef5e0b10c77b380f27eaba
Alex Lotsman <lotsman1993@gmail.com>
2019-02-10 13:39:22 +0200

    Homework 2: Pulled up methods from ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,37 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactCreationTests {
+public class ContactCreationTests extends TestBase{
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.gecko.driver", "D:\\\\Program files\\Rep\\geckodriver.exe");
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
 
-  private void login(String username, String password) {
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-  }
 
   @Test
   public void testContactCreationTests() throws Exception {
@@ -41,75 +20,4 @@ public class ContactCreationTests {
     returtnToHomePage();
   }
 
-  private void returtnToHomePage() {
-    driver.findElement(By.linkText("home page")).click();
-  }
-
-  private void submitContactForm() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-    driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    driver.findElement(By.name("address")).click();
-    driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    driver.findElement(By.name("home")).click();
-    driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys(contactData.getHomePhoneNumber());
-    driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhoneNumber());
-  }
-
-  private void initContactCreation() {
-    driver.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,21 +1,124 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
 
 public class TestBase {
 
     protected final ApplicationManager app = new ApplicationManager();
-
-    @BeforeClass(alwaysRun = true)
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+//old test
+   /* @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterClass(alwaysRun = true)
+    */
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+      System.setProperty("webdriver.gecko.driver", "D:\\\\Program files\\Rep\\geckodriver.exe");
+      driver = new FirefoxDriver();
+      baseUrl = "https://www.katalon.com/";
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      driver.get("http://localhost/addressbook/");
+      login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      driver.findElement(By.name("user")).click();
+      driver.findElement(By.name("user")).clear();
+      driver.findElement(By.name("user")).sendKeys(username);
+      driver.findElement(By.name("pass")).click();
+      driver.findElement(By.name("pass")).clear();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+    }
+
+    protected void returtnToHomePage() {
+      driver.findElement(By.linkText("home page")).click();
+    }
+
+    protected void submitContactForm() {
+      driver.findElement(By.name("submit")).click();
+    }
+
+    protected void fillContactForm(ContactData contactData) {
+      driver.findElement(By.name("firstname")).click();
+      driver.findElement(By.name("firstname")).clear();
+      driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+      driver.findElement(By.name("lastname")).click();
+      driver.findElement(By.name("lastname")).clear();
+      driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+      driver.findElement(By.name("address")).click();
+      driver.findElement(By.name("address")).clear();
+      driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      driver.findElement(By.name("home")).click();
+      driver.findElement(By.name("home")).clear();
+      driver.findElement(By.name("home")).sendKeys(contactData.getHomePhoneNumber());
+      driver.findElement(By.name("mobile")).click();
+      driver.findElement(By.name("mobile")).clear();
+      driver.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhoneNumber());
+    }
+
+    protected void initContactCreation() {
+      driver.findElement(By.linkText("add new")).click();
+    }
+//old test
+   /* @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
+    */
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+      driver.quit();
+      String verificationErrorString = verificationErrors.toString();
+      if (!"".equals(verificationErrorString)) {
+        fail(verificationErrorString);
+      }
+    }
 
+    private boolean isElementPresent(By by) {
+      try {
+        driver.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+
+    private boolean isAlertPresent() {
+      try {
+        driver.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+
+    private String closeAlertAndGetItsText() {
+      try {
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        if (acceptNextAlert) {
+          alert.accept();
+        } else {
+          alert.dismiss();
+        }
+        return alertText;
+      } finally {
+        acceptNextAlert = true;
+      }
+    }
 }
