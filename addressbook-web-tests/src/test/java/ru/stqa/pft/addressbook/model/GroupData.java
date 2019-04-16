67017bcaa19f2c91b5d842e7de71ca1e3a90b8fb
Alexandr Lotsman <lotsman1993@gmail.com>
2019-04-16 15:16:46 +0300

    Fluent-interfaces

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,28 +3,30 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private int id;
-    private final String name;
-    private final String header;
-    private final String footer;
+    private int id = Integer.MAX_VALUE;
+    private  String name;
+    private  String header;
+    private  String footer;
 
-    public GroupData(String name, String header, String footer) {
-        this.id = Integer.MAX_VALUE;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-
-    public GroupData(int id, String name, String header, String footer) {
+    public GroupData withId(int id) {
         this.id = id;
+        return this;
+    }
+
+    public GroupData withName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public GroupData withHeader(String header) {
         this.header = header;
+        return this;
+    }
+
+    public GroupData withFooter(String footer) {
         this.footer = footer;
+        return this;
     }
 
     public int getId() {
@@ -49,6 +51,11 @@ public class GroupData {
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     @Override
