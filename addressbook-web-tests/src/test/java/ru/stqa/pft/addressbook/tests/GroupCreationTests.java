e08b6b793b1498e94478303b7b85f79ecc2ebbdc
Alexandr Lotsman <lotsman1993@gmail.com>
2019-06-13 16:58:00 +0300

    Add logs

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,13 +4,14 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -53,7 +54,7 @@ public class GroupCreationTests extends TestBase {
   }
 
   @Test(dataProvider = "validGroupsFromJson")
-  public void testGroupCreation(GroupData group) throws Exception {
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -63,7 +64,7 @@ public class GroupCreationTests extends TestBase {
   }
 
   @Test(enabled = false)
-  public void testBadGroupCreation() throws Exception {
+  public void testBadGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
     GroupData group = new GroupData().withName("test'");
