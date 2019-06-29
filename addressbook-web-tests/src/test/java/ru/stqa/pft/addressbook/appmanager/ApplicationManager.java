8055b447d780b7ab21c90f94d3ed347f11a3d2a6
Alex Lotsman <lotsman1993@gmail.com>
2019-06-29 11:42:37 +0300

    Modify GroupModificationTests to read data from db

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -27,6 +27,7 @@ public class ApplicationManager {
     private String baseUrl;
     private StringBuffer verificationErrors = new StringBuffer();
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -36,6 +37,9 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
+
         if ( browser.equals(BrowserType.FIREFOX)){
             driver = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)){
@@ -68,5 +72,9 @@ public class ApplicationManager {
 
     public ContactHelper contact() {
         return contactHelper;
+    }
+
+    public DbHelper db(){
+        return  dbHelper;
     }
 }
