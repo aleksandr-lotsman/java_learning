4c4377514b7992098d1a65f5e047695bce6c4dde
Alexandr Lotsman <lotsman1993@gmail.com>
2019-07-08 20:06:38 +0300

    Add new module for Mantis tests

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class ApplicationManager {
+
+    private final Properties properties;
+    WebDriver driver;
+
+    private String baseUrl;
+    private StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
+        if ( browser.equals(BrowserType.FIREFOX)){
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        driver.get(properties.getProperty("web.baseUrl"));
+    }
+
+    public void stop() {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+          fail(verificationErrorString);
+        }
+    }
+
+}
